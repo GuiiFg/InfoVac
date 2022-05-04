6a7fbcfca80798b54a5bf451fb9b851b96d7a6ef
Guilherme <farias._02f@outlook.com>
2022-05-04 14:26:15 -0300

    semi final app settings

---
+++ app/src/main/java/com/example/infovacs/ui/aboutproject/AboutprojectFragment.java
@@ -0,0 +1,35 @@
+package com.example.infovacs.ui.aboutproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.infovacs.databinding.AboutProjectBinding;
+
+public class AboutprojectFragment extends Fragment {
+
+    private AboutProjectBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle saveInstanceState) {
+
+        AboutprojectViewModel aboutprojectViewModel =
+                new ViewModelProvider(this).get(AboutprojectViewModel.class);
+
+        binding = AboutProjectBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
